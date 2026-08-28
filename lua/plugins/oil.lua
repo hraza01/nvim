0ed1bea7575fd1b75870890f1ec462fb6733a3b2
Hasan Raza <razahasan24@gmail.com>
2026-08-28 17:20:57 -0400

    feat(oil): update cwd to follow oil navigation
    Add BufEnter autocmd for oil buffers that syncs the working directory to
    the displayed directory, so overseer task lookup and other tools follow
    navigation.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -9,3 +9,15 @@ require('oil').setup { view_options = { show_hidden = true } }
 
 vim.keymap.set('n', '-', function() require('oil').open() end, { desc = 'Oil: open parent dir' })
 vim.keymap.set('n', '<leader>-', function() require('oil').open_float() end, { desc = 'Oil: floating explorer' })
+
+-- Follow oil navigation: update cwd to the displayed directory.
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = vim.api.nvim_create_augroup('oil-auto-cd', { clear = true }),
+  pattern = 'oil:///*',
+  callback = function()
+    local dir = require('oil').get_current_dir()
+    if dir and dir ~= vim.fn.getcwd() then
+      vim.cmd('cd ' .. vim.fn.fnameescape(dir))
+    end
+  end,
+})

f10e3c151073fb91540cb883a021a52718e01656
Hasan Raza <razahasan24@gmail.com>
2026-08-28 17:20:57 -0400

    feat(overseer): auto-open interactive terminal split and enlarge task list
    Configure overseer to open a focused horizontal terminal split on task
    start (VSCode-style) and increase the bottom dock height to 40% of the
    screen.

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,7 +1,26 @@
 -- Overseer: task runner (reads .vscode/tasks.json).
 vim.pack.add { 'https://github.com/stevearc/overseer.nvim' }
 
-require('overseer').setup {}
+require('overseer').setup {
+  task_list = {
+    direction = 'bottom',
+    max_height = { 0.4 },
+    min_height = 15,
+  },
+  output = { use_terminal = true },
+  component_aliases = {
+    default = {
+      'on_exit_set_status',
+      'on_complete_notify',
+      { 'on_complete_dispose', require_view = { 'SUCCESS', 'FAILURE' } },
+      { 'open_output', on_start = 'always', direction = 'horizontal', focus = true },
+    },
+    default_vscode = {
+      'default',
+      'on_result_diagnostics',
+    },
+  },
+}
 
 vim.keymap.set('n', '<leader>or', '<Cmd>OverseerRun<CR>', { desc = '[O]verseer [r]un task' })
 vim.keymap.set('n', '<leader>oo', '<Cmd>OverseerToggle<CR>', { desc = '[O]verseer [o]utput toggle' })
